6e249813b2eed7c500039d162487a0d00ddfbf44
abhishek <abhishek.satapathy01@gmail.com>
2019-03-29 03:17:31 +0530

    Made login

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 // Imports
 // =======
 var express = require("express");
-var app = express();
-
+const bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var session = require('express-session');
 var rootRoute = require('./root.js');
+
+var app = express();
 
 // =================
 // App configuration
@@ -12,6 +15,21 @@ var rootRoute = require('./root.js');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
+//connect to mongo db
+mongoose.connect('mongodb://localhost/weather'); 
+var db = mongoose.connection;
+
+//handle mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log("connected");
+});
+
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 // Routes
 app.use(rootRoute);
--- root.js
+++ root.js
@@ -1,9 +1,27 @@
-
-
 var router = require("express").Router();
+var User = require('./models/user');
 
 router.get("/", function (req, res) {
 	res.render("index.ejs");
 });
+
+router.get("/login", function (req, res) {
+	res.render("login.ejs");
+})
+
+router.get("/register", function (req, res) {
+	res.render("register.ejs");
+})
+
+router.post("/register/user", function (req, res) {
+		var userData = {
+			email: req.body.user.email,
+			user: req.body.user.username,
+			password: req.body.user.password,
+		}
+		
+})
+
+
 
 module.exports = router;
